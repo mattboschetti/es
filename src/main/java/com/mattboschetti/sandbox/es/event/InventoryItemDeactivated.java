878bed9b61ab505c8a7901f2a5d1b29bd658f862
Matheus Boschetti <Matheus.Boschetti@nordnet.se>
2021-06-04 14:04:55 +0200

    Fix issue deserializing InventoryItemDeactivated

--- src/main/java/com/mattboschetti/sandbox/es/event/InventoryItemDeactivated.java
+++ src/main/java/com/mattboschetti/sandbox/es/event/InventoryItemDeactivated.java
@@ -1,10 +1,16 @@
 package com.mattboschetti.sandbox.es.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import java.util.UUID;
 
 public class InventoryItemDeactivated extends Event {
     public final UUID id;
 
+    // Heuristics for finding single argument constructors have some weird behavior on jackson
+    // need to explicitly tell which constructor to use
+    // https://github.com/FasterXML/jackson-databind/issues/1498
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public InventoryItemDeactivated(UUID id) {
         this.id = id;
     }
